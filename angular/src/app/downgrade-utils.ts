a35fb61eacbfbea8c2855d9040b7afa4ee45f153
Kunal Sharma <kunal.sharma@firstinsight.com>
2021-02-28 01:17:29 +0530

    include custom material theme

---
+++ angular/src/app/downgrade-utils.ts
@@ -0,0 +1,8 @@
+import {downgradeComponent, downgradeInjectable} from '@angular/upgrade/static';
+
+// @ts-ignore
+declare var angular: angular.IAngularStatic;
+
+export function downgradeItems(): void {
+
+}
