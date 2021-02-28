8bc12575d7b659afbbd324d11a7fd59c1ebd29ee
Kunal Sharma <kunal.sharma@firstinsight.com>
2021-02-28 16:25:12 +0530

    downgrade datePicker and alert service

--- angular/src/app/downgrade-utils.ts
+++ angular/src/app/downgrade-utils.ts
@@ -1,5 +1,7 @@
-import { downgradeComponent } from '@angular/upgrade/static';
+import { downgradeComponent, downgradeInjectable } from '@angular/upgrade/static';
 import { LoginComponent } from './login/login.component';
+import { DatePickerComponent } from './shared/date-picker/date-picker.component';
+import { AlertService } from './core/services/alert.service';
 
 // @ts-ignore
 declare var angular: angular.IAngularStatic;
@@ -7,5 +9,13 @@ declare var angular: angular.IAngularStatic;
 export function downgradeItems(): void {
 
   angular.module('myApp.login')
+    // download login component
     .directive('dgdLoginComponent', downgradeComponent({component: LoginComponent}))
+
+    // downgrade datePicker component
+    .directive('dgdDatePicker', downgradeComponent({component: DatePickerComponent}));
+
+  // downgrade alert service
+  angular.module('myApp.register')
+    .factory('dgdAlertService', downgradeInjectable(AlertService))
 }
