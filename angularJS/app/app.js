c60fd6c5c870e46448a6e8d3e98c258035a8e39d
Kunal Sharma <kunal.sharma@firstinsight.com>
2021-02-28 16:27:18 +0530

    configure materialJS theme for angular app

--- angularJS/app/app.js
+++ angularJS/app/app.js
@@ -2,11 +2,18 @@
 
 // Declare app level module which depends on views, and core components
 angular.module('myApp', [
+    'ngMaterial',
     'ngRoute',
     'myApp.login',
     'myApp.register'
-]).config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
-    $locationProvider.hashPrefix('!');
+]).config(['$locationProvider', '$routeProvider', '$mdThemingProvider', function ($locationProvider, $routeProvider, $mdThemingProvider) {
 
+    // theme configs
+    $mdThemingProvider.theme('default')
+        .primaryPalette('indigo')
+        .accentPalette('pink');
+
+    // route connfigs
+    $locationProvider.hashPrefix('!');
     $routeProvider.otherwise({redirectTo: '/register'});
 }]);
