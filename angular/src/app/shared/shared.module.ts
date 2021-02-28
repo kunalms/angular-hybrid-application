db4089dfcc905401b635870c5de3b8e0aaff9047
Kunal Sharma <kunal.sharma@firstinsight.com>
2021-02-28 12:50:50 +0530

    add shared module

---
+++ angular/src/app/shared/shared.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FlexLayoutModule,
+    ReactiveFormsModule
+  ], exports: [
+    FlexLayoutModule,
+    ReactiveFormsModule
+  ]
+})
+export class SharedModule {
+}
