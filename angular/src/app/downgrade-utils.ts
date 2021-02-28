e948855c12e5783c4d26d71cd5ac550c7f9d9b4b
Kunal Sharma <kunal.sharma@firstinsight.com>
2021-02-28 13:05:22 +0530

    downGrade login component

--- angular/src/app/downgrade-utils.ts
+++ angular/src/app/downgrade-utils.ts
@@ -1,8 +1,11 @@
-import {downgradeComponent, downgradeInjectable} from '@angular/upgrade/static';
+import { downgradeComponent } from '@angular/upgrade/static';
+import { LoginComponent } from './login/login.component';
 
 // @ts-ignore
 declare var angular: angular.IAngularStatic;
 
 export function downgradeItems(): void {
 
+  angular.module('myApp.login')
+    .directive('dgdLoginComponent', downgradeComponent({component: LoginComponent}))
 }
